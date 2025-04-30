ed6c604038bb3109bb2cba10e7539cac81e21b98
surajskhar074 <sekharsuraj074@gmail.com>
2025-04-30 15:13:52 +0530

    Initial commit

--- HospitalManagement/settings.py
+++ HospitalManagement/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9etypjz6%hz29_w@2+o_+mwr8xndx!s$i4vh_gslaqc_zjv#7w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
