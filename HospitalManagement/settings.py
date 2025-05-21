22386fe5c67cb835e8e57887349c09ab86d37f48
suraj sekhar mallick <sekharsurajsekhar074@gmail.com>
2025-05-21 16:13:02 +0530

    updated project with new changes

--- HospitalManagement/settings.py
+++ HospitalManagement/settings.py
@@ -33,7 +33,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 from decouple import config
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+import os
+
+DEBUG = os.getenv("DEBUG", "False") == "True"
+
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1').split(',')
 
 
